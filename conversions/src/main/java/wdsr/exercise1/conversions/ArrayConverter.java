e0c8c02f059aa4c3a3427962e67909204871eb7f
piotrfatner <fatner.piotr@gmail.com>
2017-03-08 23:19:55 +0100

    Added log4j dependences and logging in class

--- conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
+++ conversions/src/main/java/wdsr/exercise1/conversions/ArrayConverter.java
@@ -2,11 +2,17 @@ package wdsr.exercise1.conversions;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Marek on 14.02.2016.
  */
 public class ArrayConverter {
+    public static final Logger log = LogManager.getLogger();
     public int[] convertToInts(String[] strings) {
+        log.debug("convertToInts entered [strings={}]", Arrays.toString(strings));
+        log.debug("convertToInts ", Arrays.stream(strings).mapToInt(Integer::valueOf).toArray());
         return Arrays.stream(strings).mapToInt(Integer::valueOf).toArray();
     }
 }
